28b1df507db7de5e017cd54b04f645fc467dc102
Arman Kocayan <arman@armankocayan.com>
2023-04-08 17:13:43 +0300

    First commit.

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
+
+        int result1 = valA - valB / valC;
+        int result2 = (valA - valB) / valC;
+
+        System.out.println(result1);
+        System.out.println(result2);
+
+        int result3 = valA / valC * valD + valB;
+        int result4 = valA / (valC * (valD + valB));
+
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
